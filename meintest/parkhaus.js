83155e63cfa912e9fb51e2fd3ac1c30fc30bd36c
fuschtii <fausto.pellegrini@hotmail.ch>
2024-10-17 14:17:10 +0200

    interpretationen vom Chart

--- meintest/parkhaus.js
+++ meintest/parkhaus.js
@@ -41,7 +41,7 @@ const data = {
     labels: dayLabels, // Start mit Tagesansicht
     datasets: [
         {
-            label: 'Auslastung Hard',
+            label: 'Hard',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(54, 162, 235, 1)', // Blau
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
@@ -49,7 +49,7 @@ const data = {
             parkhausId: "zuerichparkhaushardauii"
         },
         {
-            label: 'Auslastung Uszsued',
+            label: 'Uszsued',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(255, 99, 132, 1)', // Rot
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -57,7 +57,7 @@ const data = {
             parkhausId: "zuerichparkplatzuszsued"
         },
         {
-            label: 'Auslastung Uni Irchel',
+            label: 'Uni Irchel',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(75, 192, 192, 1)', // Grün
             backgroundColor: 'rgba(75, 192, 192, 0.2)',
@@ -65,7 +65,7 @@ const data = {
             parkhausId: "zuerichparkhausuniirchel"
         },
         {
-            label: 'Auslastung Haustalgarten',
+            label: 'Haustalgarten',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(153, 102, 255, 1)', // Lila
             backgroundColor: 'rgba(153, 102, 255, 0.2)',
@@ -73,7 +73,7 @@ const data = {
             parkhausId: "zuerichparkhaustalgarten"
         },
         {
-            label: 'Auslastung Opéra',
+            label: 'Opéra',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(255, 206, 86, 1)', // Gelb
             backgroundColor: 'rgba(255, 206, 86, 0.2)',
@@ -81,7 +81,7 @@ const data = {
             parkhausId: "zuerichparkhausopéra"
         },
         {
-            label: 'Auslastung Urania',
+            label: 'Urania',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(54, 162, 235, 1)', // Blau
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
@@ -89,7 +89,7 @@ const data = {
             parkhausId: "zuerichparkhausurania"
         },
         {
-            label: 'Auslastung Parkside',
+            label: 'Parkside',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(255, 159, 64, 1)', // Orange
             backgroundColor: 'rgba(255, 159, 64, 0.2)',
@@ -97,7 +97,7 @@ const data = {
             parkhausId: "zuerichparkhausparkside"
         },
         {
-            label: 'Auslastung Helvetiaplatz',
+            label: 'Helvetiaplatz',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(199, 199, 199, 1)', // Grau
             backgroundColor: 'rgba(199, 199, 199, 0.2)',
@@ -105,7 +105,7 @@ const data = {
             parkhausId: "zuerichparkhaushelvetiaplatz"
         },
         {
-            label: 'Auslastung jelmoli',
+            label: 'Jelmoli',
             data: [], // Hier kommen die API-Daten rein
             borderColor: 'rgba(75, 192, 192, 1)', // Grün
             backgroundColor: 'rgba(75, 192, 192, 0.2)',
@@ -120,30 +120,32 @@ const config = {
     data: data,
     options: {
         responsive: true,
-        maintainAspectRatio: false, // Verhältnis nicht beibehalten, damit das Diagramm sich an den Container anpasst
+        maintainAspectRatio: false,
         plugins: {
             legend: {
                 position: 'top',
                 labels: {
-                    color: 'white',  // Farbe der Legende auf Weiß setzen
-                    usePointStyle: true  // Verwende Punkte statt Rechtecke in der Legende
+                    color: 'white',
+                    usePointStyle: true
                 }
             },
             title: {
                 display: true,
                 text: 'Stündliche Auslastung für das Parkhaus',
-                color: 'white'  // Farbe des Titels auf Weiß setzen
+                color: 'white'
             },
             tooltip: {
                 callbacks: {
                     label: function (context) {
                         const dataset = context.dataset;
+                        const parkhausName = dataset.label; // Holt den Namen des Parkhauses
                         const utilization = parseFloat(context.parsed.y).toFixed(1);
                         const maxPlaces = maxCapacity[dataset.parkhausId];
                         const freePlaces = maxPlaces - Math.round((utilization / 100) * maxPlaces);
 
                         return [
-                            `Auslastung: ${utilization}%`,
+                            `Parkhaus: ${parkhausName}`,  // Fügt den Namen des Parkhauses hinzu
+                            `${utilization}%`,
                             `Freie Plätze: ${freePlaces}`,
                             `Maximale Plätze: ${maxPlaces}`
                         ];
@@ -156,13 +158,13 @@ const config = {
                 title: {
                     display: true,
                     text: 'Auslastung (%)',
-                    color: 'white'  // Farbe des Achsentitels auf Weiß setzen
+                    color: 'white'
                 },
                 ticks: {
-                    color: 'rgba(200, 200, 200, 0.8)'  // Farbe der Y-Achsen-Beschriftungen auf ein helles Grau setzen
+                    color: 'rgba(200, 200, 200, 0.8)'
                 },
                 grid: {
-                    color: 'rgba(200, 200, 200, 0.3)',  // Gitternetzlinien in leichtem Grau mit reduzierter Deckkraft
+                    color: 'rgba(200, 200, 200, 0.3)',
                 },
                 beginAtZero: true,
                 max: 100
@@ -171,23 +173,23 @@ const config = {
                 title: {
                     display: true,
                     text: 'Stunden des Tages',
-                    color: 'white'  // Farbe des Achsentitels auf Weiß setzen
+                    color: 'white'
                 },
                 ticks: {
-                    color: 'rgba(220, 220, 220, 1)',  // Farbe der X-Achsen-Beschriftungen auf ein etwas helleres Grau ohne Transparenz setzen
+                    color: 'rgba(220, 220, 220, 1)',
                     autoSkip: true,
-                    maxTicksLimit: 12 // Begrenze die Anzahl der angezeigten Ticks, damit nur etwa alle 6 Stunden angezeigt wird
+                    maxTicksLimit: 12
                 },
                 grid: {
-                    color: 'rgba(200, 200, 200, 0.3)',  // Gitternetzlinien in leichtem Grau mit reduzierter Deckkraft
+                    color: 'rgba(200, 200, 200, 0.3)',
                 }
             }
         },
         elements: {
             point: {
-                radius: 5,  // Setzt die Punktgröße für die Datenpunkte
-                backgroundColor: 'white',  // Hintergrundfarbe der Punkte auf Weiß setzen
-                borderColor: 'white'  // Randfarbe der Punkte auf Weiß setzen
+                radius: 5,
+                backgroundColor: 'white',
+                borderColor: 'white'
             }
         }
     }
@@ -204,7 +206,7 @@ async function getOneParkhausData(parkhausId, timeframe = 'day') {
     let url = parkhausListURL + "?park_id=" + parkhausId + "&timeframe=" + timeframe;
 
     const response = await fetch(url);
-    console.log("Raw Response:", response);  // Zeigt die rohe Antwort, um zu verstehen, was kommt
+    console.log("Raw Response:", response);
 
     try {
         const data = await response.json();
@@ -213,14 +215,13 @@ async function getOneParkhausData(parkhausId, timeframe = 'day') {
             return [];
         }
 
-        // Prozentanzeige auf eine Dezimalstelle runden und das % Symbol hinzufügen
         return data.map(entry => {
             const utilization = parseFloat(entry.utilization);
             if (isNaN(utilization)) {
                 console.warn(`Invalid utilization value for parkhausId ${parkhausId}:`, entry.utilization);
-                return 0;  // Setze den Wert auf 0, wenn der Wert ungültig ist
+                return 0;
             }
-            return utilization.toFixed(1);  // Rundet die Nutzung auf eine Dezimalstelle
+            return utilization.toFixed(1);
         });
     } catch (e) {
         console.error("Fehler beim Parsen der JSON-Antwort:", e);
@@ -231,13 +232,44 @@ async function getOneParkhausData(parkhausId, timeframe = 'day') {
 // Funktion zum Aktualisieren der Chart-Labels und Daten
 function updateChart(timeframe) {
     if (timeframe === 'day') {
-        parkhausAuslastungChart.data.labels = dayLabels; // Jetzt enthält dayLabels die korrekten Uhrzeiten im Format "00:00", "01:00" usw.
+        parkhausAuslastungChart.data.labels = dayLabels; 
         parkhausAuslastungChart.options.scales.x.title.text = 'Stunden des Tages';
     } else if (timeframe === '72h') {
         parkhausAuslastungChart.data.labels = generate72hLabels();
         parkhausAuslastungChart.options.scales.x.title.text = 'Datum und Uhrzeit';
     }
-    parkhausAuslastungChart.update();  // Den Chart aktualisieren
+    parkhausAuslastungChart.update();
+    generateInterpretations(); // Interpretationen basierend auf den letzten 24 Stunden generieren
+}
+
+// Funktion zum Erzeugen der Interpretationen basierend auf den letzten 24 Stunden
+async function generateInterpretations() {
+    const timeframe = 'day'; // Immer nur die letzten 24 Stunden berücksichtigen
+    const labels = dayLabels;
+    const interpretations = [];
+
+    await Promise.all(data.datasets.map(async (dataset) => {
+        const datasetData = await getOneParkhausData(dataset.parkhausId, timeframe);
+        let isFull = false;
+        let fullTime = '';
+
+        for (let i = 0; i < datasetData.length; i++) {
+            if (datasetData[i] >= 90) {
+                isFull = true;
+                fullTime = labels[i];
+                break;
+            }
+        }
+
+        if (isFull) {
+            interpretations.push(
+                `- Parkhaus ${dataset.label} ist zum Zeitpunkt (${fullTime}) voll.`
+            );
+        }
+    }));
+
+    const interpretationContainer = document.getElementById('interpretation-container');
+    interpretationContainer.innerHTML = interpretations.length > 0 ? interpretations.join('<br>') : 'Keine besonderen Hinweise für die Auslastung.';
 }
 
 // Event-Listener für die Buttons
@@ -281,6 +313,7 @@ async function loadData(timeframe) {
     parkhausAuslastungChart.data.datasets[8].data = p_jelmoli;
 
     parkhausAuslastungChart.update();
+    generateInterpretations();  // Generiere Interpretationen nachdem die Daten vollständig geladen sind (für 24 Stunden)
 }
 
 // Initiale Daten laden (Tagesansicht)
